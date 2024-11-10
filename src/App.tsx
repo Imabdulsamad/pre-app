28277eb89acb6296a67980ced74bd1ddc6e358f8
Abdul Samad <imabdulsamad38@gmail.com>
2024-11-10 10:02:27 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Github Pages Configuration</h1>
+      <p className='mt-5'>This is me Abdul Samad</p>
+    </>
+  )
+}
+
+export default App
